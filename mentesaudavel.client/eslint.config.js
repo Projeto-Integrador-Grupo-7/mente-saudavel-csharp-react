7878f7b6bef064f8686a882b26cee3589e2a7830
Igor Gassen <igor.gassen@awright.com.br>
2025-06-04 20:54:22 -0300

    Convertendo template de typescript para javascript puro.

--- mentesaudavel.client/eslint.config.js
+++ mentesaudavel.client/eslint.config.js
@@ -1,14 +1,12 @@
-import js from '@eslint/js'
 import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
 
 export default tseslint.config(
   { ignores: ['dist'] },
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    "extends": ["react-app", "eslint:recommended"],
+    files: ['**/*.{js,jsx}'],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
